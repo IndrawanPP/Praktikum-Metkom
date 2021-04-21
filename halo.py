64c1abe9507f31cc241284800373237c719a216d
IndrawanPP <82960234+IndrawanPP@users.noreply.github.com>
2021-04-21 22:41:43 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# # Indrawan Putra Perdana
+
+# ## 20/462140/PA/20112
+
+# ### Geofisika 2020
+
+# In[2]:
+
+
+#|========================================|
+#                                         |
+#              Program halo.py            |
+# by indrawan.putraperdana@mail.ugm.ac.id |
+#                                         |
+#|========================================|
+
+
+# In[ ]:
+
+
+
+
